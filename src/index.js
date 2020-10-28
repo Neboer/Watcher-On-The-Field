766ab44f6c31ae313bf60efbfc487abb27ce15e8
Neboer <1653626705@qq.com>
2020-10-28 12:01:27 +0800

    第二版正式发行。

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 const mineflayer = require('mineflayer')
 const earlyHook = require('./hooks')
 const {keep_find_a_work_and_do} = require('./works')
+const {bot_config, login_immediate} = require('../config')
 
-
-const bot = mineflayer.createBot({
-    host: '39.99.239.158',
-    port: 25565,
-    username: 'Kiruya',
-    version: '1.16.3',
-    viewDistance: "far"
-})
+const bot = mineflayer.createBot(bot_config)
 
 bot.on("spawn", () => {
     bot.on("death", () => {
@@ -17,10 +11,9 @@ bot.on("spawn", () => {
         bot.end()
         process.exit(1)
     })
-    
-    setTimeout(() => {
-        bot.chat('/login Asdfghj1')
-        earlyHook(bot)
-        keep_find_a_work_and_do(bot, 1000)
-    }, 1000)
+    login_immediate(bot).then(() => {
+            earlyHook(bot);
+            return keep_find_a_work_and_do(bot, 1000)
+        }
+    )
 })
