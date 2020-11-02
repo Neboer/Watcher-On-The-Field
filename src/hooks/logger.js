54cf7854c613726c6e16dd0ada58eea2cb12ed50
Neboer <1653626705@qq.com>
2020-11-02 12:05:15 +0800

    bf

--- src/hooks/logger.js
+++ src/hooks/logger.js
@@ -6,7 +6,7 @@ const myFormat = printf(({level, message, timestamp}) => {
 });
 
 const logger = createLogger({
-    level: 'error',
+    level: 'info',
     format: format.combine(
         errors({ stack: true }),
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
--- src/message/message_resolver.js
+++ src/message/message_resolver.js
@@ -2,9 +2,8 @@ function get_chat_message(json_mes) {
     // console.log(JSON.stringify(json_mes))
     let data
     try {
-        data = ['', json_mes.with[0].json.hoverEvent.contents.name.text, json_mes.json.with[1].text]
-        console.log(data)
         data = json_mes.extra[1].text.match(/§6\[§e私聊§6] §6§4(.*?) §4§c私聊:§7 §7(.*?)§7/)
+        // data = ['', json_mes.with[0].json.hoverEvent.contents.name.text, json_mes.json.with[1].text]
     } catch (e) {
     }
     if (data) {
